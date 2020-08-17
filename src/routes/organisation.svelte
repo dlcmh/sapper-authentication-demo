f02bda87a1ecef9af253abab87c72d1e9bf2b178
David Chin <dlcmhd@me.com>
2020-08-17 13:40:30 +0800

    to be removed

--- src/routes/organisation.svelte
+++ src/routes/organisation.svelte
@@ -1,3 +1,14 @@
+<script context="module">
+  // import { stores } from '@sapper/app';
+  // const { session } = stores();
+  export async function preload(p1, p2, p3) {
+    console.log('lol');
+    console.log(p1);
+    console.log(p2);
+    console.log(p3);
+  }
+</script>
+
 <script>
   import { stores } from '@sapper/app';
 
